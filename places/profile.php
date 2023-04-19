5c272e5106134cf7f342c5d50eff66d4478171a7
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-19 01:17:28 -0400

    profile layout

--- places/profile.php
+++ places/profile.php
@@ -3,10 +3,65 @@
     include '../footer.php';
     $user = $_GET['user'];
 ?>
-<div class="cell medium-10 large-10">
-    <div class="grid-x grid-padding-x" id= "pets">
-        <div class="cell large-4" id="pagetitle">
-            <h2><?php echo $user ?>'s profile</h2>      
+<div class="cell small-10 medium-10 large-10">
+    <div class="grid-x grid-padding-x" id = "profile">
+        <div class="cell" id="pagetitle">
+            <h2><?php echo $user ?>'s profile</h2> 
+        </div>
+        <div class ="cell">
+
+        <!-- userinfo -->
+            <div class="grid-x grid-padding-x" id="userinfo">
+                <!-- will be from ajax -->
+                <div class = "cell large-6 medium-6 small-6">
+                    <h3 class="subheader">User Information</h3>
+                    <table>
+                        <tr>
+                            <td><b>Username:</b></td>
+                        </tr>
+                        <tr>
+                            <td><b>Email:</b></td>
+                        </tr>
+                        <tr>    
+                            <td><b>Age:</b></td>
+                        </tr>
+                        <tr>    
+                            <td><b>Join date:</b></td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class = "cell large-6 medium-6 small-6">
+                    <h3 class="subheader">About</h3>
+                    <div class = "callout" id="about"> </div>
+                </div>
+                <!-- /will be from ajax -->
+            </div>
+        <!-- /userinfo -->
+
+        <!-- usersubmissions -->
+            <div class="grid-x grid-padding-x">
+                <!-- from ajax -->
+                <div class="callout-wrapper">
+                    <h4 class="subheader">Submissions</h4>
+                    <div class="callout" id ="index">
+                        <div class="image-container" id = "usersub"><!-- from ajax --></div>
+                    </div>
+                </div>
+            </div>
+        <!-- /usersubmissions -->
+
+        <!-- userfavorites -->
+            <div class="grid-x grid-padding-x">
+                <div class="callout-wrapper">
+                    <h4 class="subheader">Favorites</h4>
+                    <div class="callout" id ="index">
+                        <div class="image-container" id = "userfav"><!-- from ajax --></div>
+                    </div>
+                </div>
+            </div>
+        <!-- /usersubmissions -->
+        </div>
     </div>
 </div>
 
